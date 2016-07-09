8ce25852c9e1b544902a421d2f3b01252893df73
Yongbo Zeng <yongbozeng@ele.uri.edu>
2016-07-09 11:05:17 -0700

    use webot context

--- webot.py
+++ webot.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
-import webot_core, response_handler
+import webot_core, response_handler, webot_context
 
 if __name__ == '__main__':
     res_handler = response_handler.ResponseHandler()
+    context = webot_context.WebotContext()
     webot = webot_core.WebotCore()
-    webot.start(res_handler)
+
+    webot.start(res_handler, context)
